1a79f66c52da470ea658cbec821087acab42a393
Bruno Testaguzza Carlin <brunotestaguzza@gmail.com>
2022-02-27 23:20:19 -0300

    roll back to netlify tracking

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1WNTF2E4ZT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1WNTF2E4ZT');
+</script>
